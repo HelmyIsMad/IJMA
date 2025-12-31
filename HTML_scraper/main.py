c60d23e87e534fd6a6ebca75c4c821ea7e624ddb
Mohamed Helmy <Mohamedlegendary2005@gmail.com>
2025-12-31 05:32:41 +0200

    Fix HTML scraper to return research_type

--- HTML_scraper/main.py
+++ HTML_scraper/main.py
@@ -2,6 +2,7 @@
 from .extractors import (
     _extract_code,
     _extract_title,
+    _extract_research_type,
     _extract_receive_date,
     _extract_acceptance_date,
     _extract_authors_emails_and_affiliations,
@@ -13,15 +14,16 @@ def scrape_html(html_str: str):
     """Parse IJMA submission HTML and return extracted fields using XPath.
 
     Returns:
-        (code, title, receive_date, acceptance_date, authors, emails, affiliations)
+        (code, title, research_type, receive_date, acceptance_date, authors, emails, affiliations)
     """
     page = lxml_html.fromstring(html_str)
 
     code = _extract_code(page)
     title = _extract_title(page)
+    research_type = _extract_research_type(page)
     receive_date = _extract_receive_date(page)
     acceptance_date = _extract_acceptance_date(page)
 
     authors, emails, affiliations = _extract_authors_emails_and_affiliations(page)
 
-    return code, title, receive_date, acceptance_date, authors, emails, affiliations
+    return code, title, research_type, receive_date, acceptance_date, authors, emails, affiliations
